52c8e55455945afe458c4a163dd497c488e74ca1
7sDream <i@7sdre.am>
2024-12-21 06:16:41 +0800

    refactor(day20): rename variables/functions to make it more clear

--- tests/day20.rs
+++ tests/day20.rs
@@ -5,7 +5,6 @@ mod tests {
     use advent2024::*;
 
     #[derive(Debug, Clone, Copy)]
-    #[repr(u8)]
     enum Tile {
         Empty,
         Wall,
@@ -39,13 +38,11 @@ mod tests {
                     return (Some(score), s);
                 }
 
-                let current_tile = self.map[pos.0][pos.1];
+                let tile = self.map[pos.0][pos.1];
 
-                match current_tile {
+                match tile {
                     Tile::Empty => {
-                        let neighbors = neighbors_limited(pos, self.size).into_iter().flatten();
-
-                        for neighbor in neighbors {
+                        for neighbor in neighbors_limited(pos, self.size).into_iter().flatten() {
                             let to_tile = self.map[neighbor.0][neighbor.1];
                             let entry = s.entry(neighbor);
                             if matches!(to_tile, Tile::Wall) || matches!(entry, Entry::Occupied(_))
@@ -63,9 +60,9 @@ mod tests {
             (None, s)
         }
 
-        fn cheat_points(&self, start: Pos, max_jump: usize) -> Vec<(Pos, Pos, usize)> {
+        fn cheat_points(&self, start: Pos, max_cheat: usize) -> Vec<(Pos, Pos, usize)> {
             let mut result = Vec::new();
-            let max_jump_isize = max_jump as isize;
+            let max_cheat_isize = max_cheat as isize;
 
             for row in 0..self.size.0 {
                 for col in 0..self.size.1 {
@@ -73,37 +70,41 @@ mod tests {
                         continue;
                     }
 
-                    let jump_start = (row, col);
-
-                    let Some(l_half) = self.race(start, Some(jump_start), None).0 else {
+                    let cheat_start = (row, col);
+                    let Some(start_to_cheat_start) = self.race(start, Some(cheat_start), None).0
+                    else {
                         continue;
                     };
 
-                    let reachable = self.race(jump_start, None, Some(max_jump)).1;
+                    let reachable = self.race(cheat_start, None, Some(max_cheat)).1;
 
-                    for ox in -max_jump_isize..=max_jump_isize {
-                        let y_offset_max = max_jump_isize - ox.abs();
+                    for ox in -max_cheat_isize..=max_cheat_isize {
+                        let y_offset_max = max_cheat_isize - ox.abs();
                         for oy in -y_offset_max..=y_offset_max {
-                            let jump_len = ox.unsigned_abs() + oy.unsigned_abs();
-                            if jump_len != 0 {
-                                let jump_end_y = row as isize + oy;
-                                let jump_end_x = col as isize + ox;
-                                if jump_end_x >= 0
-                                    && jump_end_y >= 0
-                                    && jump_end_y < self.size.0 as isize
-                                    && jump_end_x < self.size.1 as isize
+                            let cheat_len = ox.unsigned_abs() + oy.unsigned_abs();
+                            if cheat_len != 0 {
+                                let cheat_end_y = row as isize + oy;
+                                let cheat_end_x = col as isize + ox;
+                                if cheat_end_x >= 0
+                                    && cheat_end_y >= 0
+                                    && cheat_end_y < self.size.0 as isize
+                                    && cheat_end_x < self.size.1 as isize
                                 {
-                                    let jump_end = (jump_end_y as usize, jump_end_x as usize);
+                                    let cheat_end = (cheat_end_y as usize, cheat_end_x as usize);
 
-                                    if matches!(self.map[jump_end.0][jump_end.1], Tile::Wall) {
+                                    if matches!(self.map[cheat_end.0][cheat_end.1], Tile::Wall) {
                                         continue;
                                     }
 
                                     if reachable
-                                        .get(&jump_end)
-                                        .is_none_or(|score| jump_len < *score)
+                                        .get(&cheat_end)
+                                        .is_none_or(|score| cheat_len < *score)
                                     {
-                                        result.push((jump_start, jump_end, l_half + jump_len));
+                                        result.push((
+                                            cheat_start,
+                                            cheat_end,
+                                            start_to_cheat_start + cheat_len,
+                                        ));
                                     }
                                 }
                             }
@@ -115,25 +116,26 @@ mod tests {
             result
         }
 
-        fn cheat_race(
+        fn cheat_saves(
             &self,
             start: Pos,
             end: Pos,
-            max_jump: usize,
+            max_cheat: usize,
             no_cheat_score: usize,
         ) -> Vec<(Pos, Pos, usize)> {
             let mut to_end_scores = HashMap::<Pos, Option<usize>>::new();
 
-            self.cheat_points(start, max_jump)
+            self.cheat_points(start, max_cheat)
                 .into_iter()
-                .filter_map(|(jump_start, jump_end, left_half_score)| {
+                .filter_map(|(cheat_start, cheat_end, start_to_cheat_end_score)| {
                     to_end_scores
-                        .entry(jump_end)
-                        .or_insert_with(|| self.race(jump_end, Some(end), None).0)
-                        .and_then(|right_half| {
-                            no_cheat_score.checked_sub(right_half + left_half_score)
+                        .entry(cheat_end)
+                        .or_insert_with(|| self.race(cheat_end, Some(end), None).0)
+                        .and_then(|cheat_end_to_end_score| {
+                            no_cheat_score
+                                .checked_sub(start_to_cheat_end_score + cheat_end_to_end_score)
                         })
-                        .map(|saved| (jump_start, jump_end, saved))
+                        .map(|saved| (cheat_start, cheat_end, saved))
                 })
                 .collect()
         }
@@ -176,10 +178,10 @@ mod tests {
         )
     }
 
-    fn test(max_jump: usize) -> usize {
+    fn cheat_save_more_then_100(max_cheat: usize) -> usize {
         let (start, end, maze) = data("tests/data/day20.input.txt");
         let no_cheat = maze.race(start, Some(end), None).0.unwrap();
-        let cheats_saves = maze.cheat_race(start, end, max_jump, no_cheat);
+        let cheats_saves = maze.cheat_saves(start, end, max_cheat, no_cheat);
         cheats_saves
             .into_iter()
             .filter(|(_, _, x)| *x >= 100)
@@ -188,11 +190,11 @@ mod tests {
 
     #[test]
     fn part1() {
-        assert_eq!(test(2), 1286);
+        assert_eq!(cheat_save_more_then_100(2), 1286);
     }
 
     #[test]
     fn part2() {
-        assert_eq!(test(20), 989316);
+        assert_eq!(cheat_save_more_then_100(20), 989316);
     }
 }
